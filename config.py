354013081a6aa6d59177cd38f899ab5bdcf26476
EvanHanna12 <45378721+EvanHanna12@users.noreply.github.com>
2019-07-16 12:32:03 -0700

    Update and rename example_config.py to config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+
+import discord
+
+
+token = 'NTk2NzI1NzAyNTkwNDY0MDEz.XR9wbg.p2UdspUZn8okuQwPa9_mq2Ubztg'
+
+prefix = 'dg!'
+owner_id = 466672305989222415
+
+giveaway_channel = 600141153328693248
+giveaway_emoji = 384394753749417996
+
+giveaway_duration = datetime.timedelta(days=1)
+
+# channels in which the bot responds to commands, all others are ignored
+command_channels = {600155603213484077}
+
+activity = discord.Game('dady bots')
